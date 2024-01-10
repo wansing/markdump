d119f5ba0400d6df7e4e69306290540ac42a8d34
wansing <55762399+wansing@users.noreply.github.com>
2024-01-10 18:53:17 +0100

    feat: create random secret

--- cmd/markdump/main.go
+++ cmd/markdump/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"log"
 	"net/http"
 	"os"
@@ -14,6 +16,15 @@ func main() {
 	listen := os.Getenv("LISTEN")
 	if listen == "" {
 		listen = "127.0.0.1:8134"
+	}
+	secret := os.Getenv("SECRET")
+	if secret == "" {
+		var bs = make([]byte, 16)
+		if _, err := rand.Read(bs); err != nil {
+			log.Fatalf("error making random secret: %v", err)
+		}
+		secret = base64.RawURLEncoding.EncodeToString(bs)
+		log.Printf("generated temporary reload secret: %s", secret)
 	}
 
 	srv := &markdump.Server{
